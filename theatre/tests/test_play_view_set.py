e44c6a8e02a42e057e40af4a2391c9c617166734
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-11-10 18:54:08 +0100

    theatre/tests/test_play_view_set.py

--- theatre/tests/test_play_view_set.py
+++ theatre/tests/test_play_view_set.py
@@ -5,28 +5,26 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 
-from cinema.models import Movie, CinemaHall, Genre, Actor
-from cinema.serializers import (
-    MovieListSerializer,
-    MovieDetailSerializer,
-    MovieImageSerializer,
-    MovieSerializer,
+from theatre.models import Play, TheatreHall, Genre, Actor
+from theatre.serializers import (
+    PlayListSerializer,
+    PlayDetailSerializer,
+    PlaySerializer,
 )
-from cinema.views import MovieViewSet
+from theatre.views import PlayViewSet
 
-MOVIE_URL = reverse("cinema:movie-list")
-MOVIE_SESSION_URL = reverse("cinema:moviesession-list")
+PlAY_URL = reverse("theatre:play-list")
+MOVIE_SESSION_URL = reverse("theatre:performance-list")
 
 
-def sample_movie(**params):
+def sample_play(**params):
     defaults = {
-        "title": "Sample movie",
+        "title": "Sample play",
         "description": "Sample description",
-        "duration": 90,
     }
     defaults.update(params)
 
-    return Movie.objects.create(**defaults)
+    return Play.objects.create(**defaults)
 
 
 def sample_genre(**params):
@@ -39,24 +37,24 @@ def sample_genre(**params):
 
 
 def sample_actor(**params):
-    defaults = {"first_name": "George", "last_name": "Clooney"}
+    defaults = {"first_name": "John", "last_name": "Doe"}
     defaults.update(params)
 
     return Actor.objects.create(**defaults)
 
 
-def sample_movie_session(**params):
-    cinema_hall = CinemaHall.objects.create(name="Blue", rows=20, seats_in_row=20)
+def sample_performance(**params):
+    theatre_hall = TheatreHall.objects.create(name="Blue", rows=20, seats_in_row=20)
 
     defaults = {
         "show_time": "2022-06-02 14:00:00",
-        "movie": None,
-        "cinema_hall": cinema_hall,
+        "play": None,
+        "theatre_hall": theatre_hall,
     }
     defaults.update(params)
 
 
-class MovieViewSetTests(APITestCase):
+class PlayViewSetTests(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
@@ -65,55 +63,52 @@ class MovieViewSetTests(APITestCase):
         self.client.force_authenticate(self.user)
         self.genre = sample_genre()
         self.actor = sample_actor()
-        self.movie = sample_movie()
-        self.movie.genres.add(self.genre)
-        self.movie.actors.add(self.actor)
-        self.movie.save()
+        self.play = sample_play()
+        self.play.genres.add(self.genre)
+        self.play.actors.add(self.actor)
+        self.play.save()
 
     def test_list_movies(self):
-        url = (MOVIE_URL)
+        url = PlAY_URL
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_retrieve_movie(self):
-        url = reverse("cinema:movie-detail", args=[self.movie.id])
+        url = reverse("theatre:play-detail", args=[self.play.id])
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_filter_movies_by_title(self):
-        url = (MOVIE_URL) + f"?title={self.movie.title}"
+    def test_filter_play_by_title(self):
+        url = PlAY_URL + f"?title={self.play.title}"
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["title"], self.movie.title)
+        self.assertEqual(response.data[0]["title"], self.play.title)
         self.assertIsNot(response.data[0]["title"], [])
 
-    def test_filter_movies_by_genre(self):
-        url = (MOVIE_URL) + f"?genres={self.genre.id}"
+    def test_filter_play_by_genre(self):
+        url = (PlAY_URL + f"?genres={self.genre.id}")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["title"], self.movie.title)
+        self.assertEqual(response.data[0]["title"], self.play.title)
         self.assertIsNot(response.data[0]["genres"], [])
 
-    def test_filter_movies_by_actor(self):
-        url = (MOVIE_URL + f"?actors={self.actor.id}")
+    def test_filter_play_by_actor(self):
+        url = (PlAY_URL + f"?actors={self.actor.id}")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["title"], self.movie.title)
+        self.assertEqual(response.data[0]["title"], self.play.title)
         self.assertIsNot(response.data[0]["actors"], [])
 
     def test_get_serializer_class(self):
-        view = MovieViewSet()
+        view = PlayViewSet()
         view.action = "list"
-        self.assertEqual(view.get_serializer_class(), MovieListSerializer)
+        self.assertEqual(view.get_serializer_class(), PlayListSerializer)
 
         view.action = "retrieve"
-        self.assertEqual(view.get_serializer_class(), MovieDetailSerializer)
-
-        view.action = "upload_image"
-        self.assertEqual(view.get_serializer_class(), MovieImageSerializer)
+        self.assertEqual(view.get_serializer_class(), PlayDetailSerializer)
 
         view.action = ""
-        self.assertEqual(view.get_serializer_class(), MovieSerializer)
+        self.assertEqual(view.get_serializer_class(), PlaySerializer)
