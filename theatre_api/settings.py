f0f7bb362136f1535c25d0ceab110b8eee85d62f
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-11-11 22:15:09 +0100

    hided secret key

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$=w!1ulj(m1v2y58$kn^hv839fh8))zkwhvcw9&e@$9x7eaphe"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
